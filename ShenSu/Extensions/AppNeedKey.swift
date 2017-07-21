0b618317066b8b6ef420927b8213afcc4e582dc2
曾攀 <990179784@qq.com>
2017-07-21 16:14:11 +0800

    提交代码

--- ShenSu/Extensions/AppNeedKey.swift
+++ ShenSu/Extensions/AppNeedKey.swift
@@ -9,8 +9,8 @@
 import UIKit
 
 class AppNeedKey: NSObject {
-	var UMkey = "596729958f4a9d4abe00001e"
+	var UMkey = "596ec8c107fe657ca1001306"
 	var JpushKey = ""
-	var GDMapKey = "333553f9ea3972516aa6d2ecfd2b64c7"
-	var AppID = "1259101356"
+	var GDMapKey = "54f95cddad43e0466fc66c982bdfa668"
+	var AppID = "1261214397"
 }
